b09e9c663bd2143dea9fccdde4fc5b86f9239c01
ABDUL JALEL ALISMAAEL <97349470+ABDULJALEL98@users.noreply.github.com>
2024-07-14 01:43:32 +0300

    AddDbContext

---
+++ E-Commerce/Data/EcommerceDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace E_Commerce.Data;
+
+public class EcommerceDbContext:DbContext
+{
+    public EcommerceDbContext(DbContextOptions<EcommerceDbContext> options) : base(options)
+    {
+        
+    }
+}
--- E-Commerce/Startup.cs
+++ E-Commerce/Startup.cs
@@ -1,3 +1,4 @@
+using E_Commerce.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -23,6 +24,8 @@ namespace E_Commerce
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //ConfigureServices
+            services.AddDbContext<EcommerceDbContext>();
             services.AddControllersWithViews();
         }
 
