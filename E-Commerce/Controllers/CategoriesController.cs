e7c1cbc86c4e519fa1b2232c4221bb821311065f
ABDUL JALEL ALISMAAEL <97349470+ABDULJALEL98@users.noreply.github.com>
2024-07-14 17:06:54 +0300

    Ctegory Controller

---
+++ E-Commerce/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using E_Commerce.Data;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace E_Commerce.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly EcommerceDbContext _context;
+        public CategoriesController(EcommerceDbContext context)
+        {
+            _context = context;
+
+        }
+        public IActionResult Index()
+        {
+            var Response = _context.Categories.ToList();
+            return View(Response);
+        }
+    }
+}
